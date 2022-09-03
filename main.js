15b951d161fa08b37fdb4df76cd12e10d87b50b3
Asuryu <tomas-250@outlook.com>
2022-09-03 21:25:31 +0100

    got@11.8.5 -> got@12.4.1

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createWindow () {
     // and load the index.html of the app.
     mainWindow.loadFile('index.html');
   
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
