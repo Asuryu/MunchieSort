d4ae24489d70e11cad7aa3f7ef54780f2ec61e63
Asuryu <tomas-250@outlook.com>
2022-09-16 17:58:30 +0100

    Added downtime from 0AM to 00:15AM

--- api.py
+++ api.py
@@ -1,13 +1,24 @@
 import flask
 import json
+import datetime
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# check if time is between 00:00 and 00:15
+def is_time_between():
+    now = datetime.datetime.now()
+    start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    end = now.replace(hour=0, minute=15, second=0, microsecond=0)
+    return start <= now <= end
+
 @app.route('/api/v1/resources/items', methods=['GET'])
 def get_items():
-    with open("items.json", "r") as f:
-        data = json.load(f)
-    return data
+    if is_time_between():
+        return json.dumps({'error': 'service unavailable'}), 503
+    else:
+        with open("items.json", "r") as f:
+            data = json.load(f)
+        return data
 
 app.run()
