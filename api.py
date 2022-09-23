25e056b35797c892bba9db7c7d4c198c8c0116d5
Tomás Silva <tomas-250@outlook.com>
2022-09-23 14:02:29 +0100

    New API endpoints to POST and GET a bag

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
+from os import rename
 import flask
+from flask import render_template
 import json
 import datetime
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
+bags = []
+
 
 # check if time is between 00:00 and 00:15
 def is_time_between():
@@ -11,6 +17,7 @@ def is_time_between():
     start = now.replace(hour=0, minute=0, second=0, microsecond=0)
     end = now.replace(hour=0, minute=15, second=0, microsecond=0)
     return start <= now <= end
+
 
 @app.route('/api/v1/resources/items', methods=['GET'])
 def get_items():
@@ -21,4 +28,35 @@ def get_items():
             data = json.load(f)
         return data
 
-app.run()
+
+@app.route('/api/v1/resources/bag/', methods=['POST'])
+def save_bag():
+    data = flask.request.get_json()
+    if is_time_between():
+        return json.dumps({'error': 'service unavailable'}), 503
+    else:
+        # append or replace bag
+        print(str(data['id']))
+        for bag in bags:
+            if str(bag['id']) == str(data['id']):
+                bags.remove(bag)
+        bags.append(data)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        
+
+@app.route('/api/v1/resources/bag/<id>', methods=['GET'])
+def get_bag(id):
+    id = id
+    if is_time_between():
+        return json.dumps({'error': 'service unavailable'}), 503
+    else:
+        if(len(bags) > 0):
+            for bag in bags:
+                if str(bag['id']) == str(id):
+                    return render_template("bag.html", data=bag)
+            return json.dumps({'error': 'bag not found'}), 404
+        else:
+            return json.dumps({'error': 'bag not found'}), 404
+
+
+app.run(host="192.168.1.85", port=25565)
